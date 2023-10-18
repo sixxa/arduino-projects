3551f845fff846fb224f216fe16089cbee3de6f1
Tornike Sikharulidze <92145572+sixxa@users.noreply.github.com>
2023-10-18 17:30:33 +0400

    Add files via upload

---
+++ arduino stuff/fire_sensor/fire_sensor.ino
@@ -0,0 +1,53 @@
+#include<SoftwareSerial.h>
+
+int sensorPin = A0; // select the input pin for the LDR
+
+int sensorValue = 0; // variable to store the value coming from the sensor
+
+int buzzer = 12; // Output pin for Buzzer
+
+SoftwareSerial bluetoothSerial(11, 10);
+
+void setup() {
+
+// declare the ledPin and buzzer as an OUTPUT:
+
+
+pinMode(buzzer,OUTPUT);
+
+Serial.begin(9600);
+
+bluetoothSerial.begin(9600);
+
+}
+
+void loop()
+
+{
+
+Serial.println("Welcome to TechPonder Flame Sensor Tutorial");
+
+sensorValue = analogRead(sensorPin);
+
+Serial.println(sensorValue);
+
+if (sensorValue < 90) {
+
+Serial.println("Fire Detected");
+
+digitalWrite(buzzer,HIGH);
+
+delay(1000);
+
+} 
+{
+  bluetoothSerial.print(sensorValue);
+}
+
+digitalWrite(buzzer,LOW);
+
+
+delay(sensorValue);
+
+
+}
