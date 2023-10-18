1cac536b75989f729c4123b3176ac70a990c46ea
Tornike Sikharulidze <92145572+sixxa@users.noreply.github.com>
2023-10-18 19:52:09 +0400

    Add files via upload

---
+++ CardReader/CardReader.ino
@@ -0,0 +1,57 @@
+#include <SPI.h>
+#include <RFID.h>
+#include <Servo.h>
+#include <SoftwareSerial.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+
+RFID rfid(SS_PIN, RST_PIN);
+SoftwareSerial ChemiSeriali(2, 3); // RX, TX (chemi seriali 'blutuzisaTvis')
+
+int BaratNum = 0;
+String str;
+
+Servo servo8; // Declare the servo object
+
+void setup()
+{
+  Serial.begin(9600);
+  ChemiSeriali.begin(9600);
+  SPI.begin();
+  rfid.init();
+  servo8.attach(8); // Attach servo  pin 8
+  Serial.println("machvene barati");
+}
+
+void loop()
+{
+  if (rfid.isCard())
+  {
+
+    if (rfid.readCardSerial())
+    {
+      str = String(rfid.serNum[0]) + String(rfid.serNum[1]) + String(rfid.serNum[2]) + String(rfid.serNum[3]) + String(rfid.serNum[4]);
+
+      BaratNum = 0;
+      if (str == "20814713616219")
+      {
+        servo8.write(90);
+        delay(5000);
+        servo8.write(0);
+      }
+      else
+      {
+        servo8.write(0);
+      }
+
+      Serial.print(str);
+      Serial.print("    ");
+      Serial.println(BaratNum);
+      ChemiSeriali.println(BaratNum);
+      delay(100);
+    }
+  }
+  rfid.halt();
+}
+
---
+++ fire_sensor/fire_sensor.ino
@@ -0,0 +1,51 @@
+#include<SoftwareSerial.h>
+
+int sensorPin = A0; // input  LDR
+
+int sensorValue = 0; // sensor value
+
+int buzzer = 12; // Output Buzzer
+
+SoftwareSerial bluetoothSerial(11, 10);
+
+void setup() {
+
+
+pinMode(buzzer,OUTPUT);
+
+Serial.begin(9600);
+
+bluetoothSerial.begin(9600);
+
+}
+
+void loop()
+
+{
+
+Serial.println("Welcome to TechPonder Flame Sensor Tutorial");
+
+sensorValue = analogRead(sensorPin);
+
+Serial.println(sensorValue);
+
+if (sensorValue < 90) {
+
+Serial.println("Fire Detected");
+
+digitalWrite(buzzer,HIGH);
+
+delay(1000);
+
+} 
+{
+  bluetoothSerial.print(sensorValue);
+}
+
+digitalWrite(buzzer,LOW);
+
+
+delay(sensorValue);
+
+
+}
